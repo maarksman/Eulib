840481de30cd0b2f13b5cdd4567bf481878f3100
Sharma <pshar10@glsn-imac-48.ur.rochester.edu>
2017-11-10 14:45:50 -0500

    got rid of some small bugs in html

--- textscroller/scripts.js
+++ textscroller/scripts.js
@@ -3,15 +3,17 @@ $(function(){
   $('.upArrow').click(showUpperDef);
   //$('.downArrow').click(showLowerDef);
   //$('.rightArrow').click(showRightDef);
-  //$('.left').click(showLeftDef);
+  //$('.left').click(showL  eftDef);
 });
 
 
 function showUpperDef() {
+  console.log('do we come here?');
+//   $.get('/knowl1/horizontal/def1h.txt', function(data) {
+//     $('.knowl').load('<p>' + data + '</p>');
+// }, 'text');
 
-  $.get('/knowl1/horizontal/def1h.txt', function(data) {
-    $('.knowl').load('<p>' + data + '</p>');
-}, 'text');
+$.get('linkedfile3.html');
 
 
 }
