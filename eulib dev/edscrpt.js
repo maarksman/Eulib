480d8f550f0b56831e4d6a298b976945c3b0b4ac
Prikshet <pshar10@u.rochester.edu>
2017-11-30 20:06:00 -0500

    textarea works

--- eulib dev/edscrpt.js
+++ eulib dev/edscrpt.js
@@ -1,12 +1,23 @@
 $(document).ready(function()
 {
-  console.log("hello");
   var text = 'Long, long after a time there comes a wonderful day, when spring starts with a shine, you can wonder what I say';
   $('body').append('<div class = "noedit"><p>'+ text +'</p></div>');
+  $('body').append('<textarea class = "edit">'+text+' </textarea>');
+  $('body').append('<button class = "save">Save</button>');
+
+  $('.edit').hide();
 
   $('.noedit').click(function(){
-    $('.noedit').remove();
-    $('body').append('<textarea class = "edit">'+text+' </textarea>');
+    $('.noedit').hide();
+    $('.edit').show();
+  });
+
+//click save
+  $('.save').click(function(){
+    text = $('.edit').val();
+    $('.edit').hide();
+    $('.noedit').html('<p>'+ text +'</p>');
+    $('.noedit').show();
   });
 
 });
