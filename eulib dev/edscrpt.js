4384ac98e3411017023204567ec906fa13291dd3
Prikshet <pshar10@u.rochester.edu>
2017-12-01 16:48:27 -0500

    added the add knowl functionality

--- eulib dev/edscrpt.js
+++ eulib dev/edscrpt.js
@@ -1,23 +1,46 @@
 $(document).ready(function()
 {
-  var text = 'Long, long after a time there comes a wonderful day, when spring starts with a shine, you can wonder what I say';
-  $('body').append('<div class = "noedit"><p>'+ text +'</p></div>');
-  $('body').append('<textarea class = "edit">'+text+' </textarea>');
+  var text = '<p>Long, long after a time there comes a wonderful day, when spring starts with a shine, you can wonder what I say</p>';
+  $('body').append('<div class = "mNoedit">'+ text +'</div>');
+  $('body').append('<textarea class = "mEdit">'+text+' </textarea>');
+  $('body').append('<input class = "search" list = "results" ><datalist id="results"><option value = "topology.html"/><option value = "linearalgebra.html"/></datalist></input>');
+  $('body').append('<button class = "add">Add</button>');
   $('body').append('<button class = "save">Save</button>');
+  $('body').append('<button class = "edit">Edit</button>');
 
-  $('.edit').hide();
+  $('.search').hide();
+  $('.add').hide();
+  $('.mEdit').hide();
 
-  $('.noedit').click(function(){
-    $('.noedit').hide();
-    $('.edit').show();
+  $('.edit').click(function(){
+    $('.mNoedit').hide();
+    $('.mEdit').show();
+    $('.search').show();
+    $('.add').show();
+
   });
 
 //click save
   $('.save').click(function(){
-    text = $('.edit').val();
-    $('.edit').hide();
-    $('.noedit').html('<p>'+ text +'</p>');
-    $('.noedit').show();
+    text = $('.mEdit').val();
+    $('.mEdit').hide();
+    $('.mNoedit').html(text);
+    $('.mNoedit').show();
   });
+
+  //click add
+  $('.add').click(function(){
+    var link = $('.search').val();
+    var snippet = '<a knowl= " ' + link + '" href = "' + link+ '"></a>';
+    var cursorPosition = $('.mEdit').prop('selectionStart');
+    console.log(cursorPosition);
+    var start = text.substring(1,cursorPosition);
+    var end = text.substring(cursorPosition, text.length);
+    text=start + snippet + end;
+    $('.mEdit').html(text);
+
+  });
+
+
 
 });
